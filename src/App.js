17cf49bd41bfb64e56806efa098143f235b1340c
zaki.mantiqh <zaki.mantiqh@gmail.com>
2022-05-09 12:37:48 +0530

    taken a pull from nikhat branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,150 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import LeaderLine from "react-leader-line";
+import React from "react";
+import Bapp from "./Bapp";
+
+let arr = [];
 
 function App() {
+  const [startValue, setStartValue] = React.useState(null);
+  const [endValue, setEndValue] = React.useState(null);
+  const [diff, setDiff] = React.useState(true);
+  const [starts] = React.useState([
+    {
+      start: "start0",
+    },
+    {
+      start: "start1",
+    },
+    {
+      start: "start2",
+    },
+    {
+      start: "start3",
+    },
+    {
+      start: "start4",
+    },
+    {
+      start: "start5",
+    },
+    {
+      start: "start6",
+    },
+  ]);
+
+  const [ends] = React.useState([
+    {
+      end: "end0",
+    },
+    {
+      end: "end1",
+    },
+    {
+      end: "end2",
+    },
+    {
+      end: "end3",
+    },
+    {
+      end: "end4",
+    },
+    {
+      end: "end5",
+    },
+    {
+      end: "end6",
+    },
+  ]);
+
+  const addConnections = (start, end) => {
+    if (startValue && endValue) {
+      var line;
+      if (
+        arr[start.index] !== undefined &&
+        arr[start.index] !== null &&
+        arr[start.index].length > 0
+      ) {
+        line = new LeaderLine(
+          document.getElementById(start.id),
+          document.getElementById(ends[start.index].end)
+        );
+        line.hide();
+        console.log(line);
+      }
+      if (line !== undefined) {
+        console.log(line);
+        line.remove();
+      }
+      new LeaderLine(
+        document.getElementById(start.id),
+        document.getElementById(end.id)
+      );
+      arr[start.index] = [end.index];
+      // setStartValue(null);
+      setEndValue(null);
+    }
+    console.log(arr);
+  };
+
+  React.useEffect(() => {
+    addConnections(startValue, endValue);
+  }, [startValue, endValue]);
+
+  const setStart = (value, index) => {
+    setStartValue({ id: value, index: index });
+  };
+
+  const setEnd = (value, index) => {
+    setEndValue({ id: value, index: index });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div>
+        <button onClick={() => setDiff(!diff)}>Go to diff</button>
+      </div>
+      {diff ? (
+        <header className='App-header'>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p className='App-link' target='_blank' rel='noopener noreferrer'>
+            Learn React
+          </p>
+          <div className='projects'>
+            <div className='starts'>
+              {starts.map((item, index) => {
+                return (
+                  <button
+                    key={item.start}
+                    id={item.start}
+                    onClick={() => setStart(item.start, index)}>
+                    start
+                  </button>
+                );
+              })}
+            </div>
+            <div className='ends'>
+              {ends.map((item, index) => {
+                return (
+                  <button
+                    disabled={startValue === null ? true : false}
+                    id={item.end}
+                    key={item.end}
+                    onClick={() => setEnd(item.end, index)}>
+                    end
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+        </header>
+      ) : (
+        <div>
+          <Bapp />
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/Bapp.js
@@ -0,0 +1,246 @@
+import React from "react";
+import "./App.css";
+import LeaderLine from "react-leader-line";
+let arr = [];
+let lines = [];
+
+export default function Bapp() {
+  const [startValue, setStartValue] = React.useState({});
+  const [endValue, setEndValue] = React.useState({});
+  const [starts] = React.useState([
+    {
+      start: "start0",
+    },
+    {
+      start: "start1",
+    },
+    {
+      start: "start2",
+    },
+    {
+      start: "start3",
+    },
+    {
+      start: "start4",
+    },
+    {
+      start: "start5",
+    },
+    {
+      start: "start6",
+    },
+  ]);
+
+  const [ends] = React.useState([
+    {
+      end: "end0",
+    },
+    {
+      end: "end1",
+    },
+    {
+      end: "end2",
+    },
+    {
+      end: "end3",
+    },
+    {
+      end: "end4",
+    },
+    {
+      end: "end5",
+    },
+    {
+      end: "end6",
+    },
+  ]);
+
+  const addConnections = (start, end, i, j) => {
+    if (start && end) {
+      let line = new LeaderLine(
+        document.getElementById(start),
+        document.getElementById(end),
+        { hide: true }
+      );
+      lines[i][j] = line;
+      setEndValue(null);
+    }
+  };
+
+  React.useEffect(() => {
+    for (let i in starts) {
+      lines[i] = [];
+      arr[i] = [];
+      for (let j in ends) {
+        addConnections(starts[i].start, ends[j].end, i, j);
+      }
+    }
+
+    return () => {
+      for (let i in lines) {
+        for (let j in lines[i]) {
+          lines[i][j].hide();
+        }
+      }
+    };
+  }, []);
+
+  //   /**
+  //    * this useEffect is for single dimensional
+  //    * MMQ type questions
+  //    */
+  //   React.useEffect(() => {
+  //     /**
+  //      * check of endValue and startValue is empty or null
+  //      */
+  //     if (
+  //       endValue !== null &&
+  //       startValue !== null &&
+  //       Object.keys(endValue).length > 0 &&
+  //       Object.keys(startValue).length > 0
+  //     ) {
+  //       /**
+  //        * hide every line visible
+  //        */
+  //       for (let i in lines[startValue.index]) {
+  //         lines[startValue.index][i].hide();
+  //       }
+  //       /**
+  //        * check if current endValue's index is
+  //        * there inside arra elements
+  //        */
+  //       if (arr[startValue.index] === endValue.index) {
+  //         /**
+  //          * remove current index element from arr
+  //          */
+  //         arr[startValue.index] = [];
+  //         /**
+  //          * hide current selected startValue index line to
+  //          * current selected endValue index line
+  //          */
+  //         lines[startValue.index][endValue.index].hide();
+  //       } else {
+  //         /**
+  //          * assign arr of current selected startValue index with
+  //          * selected endValue index
+  //          */
+  //         arr[startValue.index] = endValue.index;
+  //         /**
+  //          * show connected line from selected startValue index
+  //          * to currently selected endValue index
+  //          */
+  //         lines[startValue.index][endValue.index].show();
+  //       }
+  //       /**
+  //        * remove empty selected startValues and endValues
+  //        */
+  //       setEndValue({});
+  //       setStartValue({});
+  //     }
+  //   });
+
+  /**
+   * this useEffect is for multi dimensional
+   * MMQ type questions
+   */
+  React.useEffect(() => {
+    /**
+     * check of endValue and startValue is empty or null
+     */
+    if (
+      endValue !== null &&
+      startValue !== null &&
+      Object.keys(endValue).length > 0 &&
+      Object.keys(startValue).length > 0
+    ) {
+      /**
+       * check if current endValue's index is
+       * there inside arra elements
+       */
+      if (arr[startValue.index] === endValue.index) {
+        /**
+         * remove current index element from arr
+         */
+        arr[startValue.index] = [];
+        /**
+         * hide current selected startValue index line to
+         * current selected endValue index line
+         */
+        lines[startValue.index][endValue.index].hide();
+      } else {
+        /**
+         * assign arr of current selected startValue index with
+         * selected endValue index
+         */
+        arr[startValue.index].push(endValue.index);
+        /**
+         * show connected line from selected startValue index
+         * to currently selected endValue index
+         */
+        lines[startValue.index][endValue.index].show();
+      }
+      /**
+       * remove empty selected startValues and endValues
+       */
+      setEndValue({});
+      setStartValue({});
+    }
+  });
+
+  /**
+   *
+   * @param {*id of the start element to be sent} value
+   * @param {*index of the start element to be sent} index
+   */
+  const setStart = (value, index) => {
+    /**
+     * assign an object to startValue
+     */
+    setStartValue({ id: value, index: index });
+  };
+
+  /**
+   * @param {*id of the end element to be sent} value
+   * @param {*index of the end element to be sent} index
+   */
+  const setEnd = (value, index) => {
+    /**
+     * assign an object to endValue
+     */
+    setEndValue({ id: value, index: index });
+  };
+
+  return (
+    <div>
+      <header className='App-header'>
+        <div className='projects'>
+          <div className='starts'>
+            {starts.map((item, index) => {
+              return (
+                <button
+                  key={item.start}
+                  id={item.start}
+                  onClick={() => setStart(item.start, index)}>
+                  start
+                </button>
+              );
+            })}
+          </div>
+          <div className='ends'>
+            {ends.map((item, index) => {
+              return (
+                <button
+                  disabled={Object.keys(startValue).length > 0 ? false : true}
+                  id={item.end}
+                  key={item.end}
+                  onClick={() => setEnd(item.end, index)}>
+                  end
+                </button>
+              );
+            })}
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
